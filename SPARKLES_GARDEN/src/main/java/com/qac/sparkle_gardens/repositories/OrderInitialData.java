02575e525ea518df443e720fdbd9c18347f72804
Damien Lloyd <damien.lloyd21@gmail.com>
2016-08-03 15:41:03 +0100

    Added documentation for impl.

--- SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/repositories/OrderInitialData.java
+++ SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/repositories/OrderInitialData.java
@@ -3,10 +3,20 @@ package com.qac.sparkle_gardens.repositories;
 import java.util.List;
 import com.qac.sparkle_gardens.entities.Order;
 
+/**
+ * OrderInitialData contains initial data that will hold orders offline.
+ * It allows you to add orders to a list thereof
+ * @author Damien Lloyd
+ *
+ */
 public class OrderInitialData 
 {
 	private List<Order> orders;
 	
+	/**
+	 * Default constructor for OrderInitialData; constructs
+	 * data from relevant entities.
+	 */
 	public OrderInitialData()
 	{
 		orders.add(new Order("OD01", 4, 40));
@@ -16,16 +26,28 @@ public class OrderInitialData
 		orders.add(new Order("OD05", 2, 10));
 	}
 	
+	/**
+	 * Add an order to list of orders
+	 * @param o
+	 */
 	public void addOrder(Order o)
 	{
 		this.orders.add(o);
 	}
 	
+	/**
+	 * Get list of orders
+	 * @return orders
+	 */
 	public List<Order> getOrders()
 	{
 		return orders;
 	}
 	
+	/**
+	 * Concurrently set all orders in a list by another
+	 * @param orders
+	 */
 	public void setOrders(List<Order> orders)
 	{
 		this.orders = orders;
--- SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/repositories/offline/OrderLineInitialData.java
+++ SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/repositories/offline/OrderLineInitialData.java
@@ -5,25 +5,45 @@ import java.util.List;
 import com.qac.sparkle_gardens.entities.Order;
 import com.qac.sparkle_gardens.entities.OrderLine;
 
+/**
+ * Bean for initial data of order line
+ * @author Damien Lloyd
+ *
+ */
 public class OrderLineInitialData 
 {
 	private List<OrderLine> orderLines;
 	
+	/**
+	 * Constructs dummy data in order line
+	 */
 	public OrderLineInitialData()
 	{
 		orderLines.add(new OrderLine(new Order("OD01", 1, 10)));
 	}
 	
+	/**
+	 * Get order list
+	 * @return
+	 */
 	public List<OrderLine> getOrderLines()
 	{
 		return orderLines;
 	}
 	
+	/**
+	 * Add orderline to list
+	 * @param ol
+	 */
 	public void addOrderLine(OrderLine ol)
 	{
 		orderLines.add(ol);
 	}
 	
+	/**
+	 * Set list of order lines to another
+	 * @param ol
+	 */
 	public void setOrderLines(List<OrderLine> ol)
 	{
 		orderLines = ol;
