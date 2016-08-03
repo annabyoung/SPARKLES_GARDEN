406b941ae65f32bbcca8ca19486f5e56a2c8baf7
Damien Lloyd <damien.lloyd21@gmail.com>
2016-08-03 14:07:35 +0100

    Added data to repositories and impl. functions

--- SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/repositories/offline/OrderRepositoryOffline.java
+++ SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/repositories/offline/OrderRepositoryOffline.java
@@ -3,47 +3,66 @@ package com.qac.sparkle_gardens.repositories.offline;
 import java.util.List;
 
 import com.qac.sparkle_gardens.entities.Order;
+import com.qac.sparkle_gardens.repositories.OrderInitialData;
 import com.qac.sparkle_gardens.repositories.OrderRepository;
 
 public class OrderRepositoryOffline 
 	implements OrderRepository
 {
+	private OrderInitialData initialData;
+	
 	public OrderRepositoryOffline() 
 	{
-		// TODO Auto-generated constructor stub
+		initialData = new OrderInitialData();
 	}
 
 	public void persistOrder(Order o) 
 	{
-		// TODO Auto-generated method stub
-		
+		initialData.addOrder(o);
 	}
 
 	public void persistOrders(List<Order> o) 
 	{
-		// TODO Auto-generated method stub
-		
+		initialData.setOrders(o);
 	}
 
 	public Order findByID(String orderID) 
 	{
-		// TODO Auto-generated method stub
-		return null;
+		List<Order> ol = initialData.getOrders();
+		Order o = null;
+		
+		for (int i = 0; i < ol.size(); i++)
+		{
+			if (ol.get(i).getOrderID() == orderID)
+				o = ol.get(i);
+		}
+		return o;
 	}
 
 	public List<Order> getOrders() 
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return initialData.getOrders();
 	}
 
 	public void updateOrder(Order o) 
 	{
-		// TODO Auto-generated method stub
+		List<Order> ol = initialData.getOrders();
+		
+		for (int i = 0; i < ol.size(); i++)
+		{
+			if (ol.get(i).equals(o))
+				ol.set(i, o);
+		}
 	}
 
 	public void removeOrder(Order o) 
 	{
-		// TODO Auto-generated method stub
+		List<Order> ol = initialData.getOrders();
+		
+		for (int i = 0; i < ol.size(); i++)
+		{
+			if (ol.get(i).equals(o))
+				ol.remove(i);
+		}
 	}
 }
