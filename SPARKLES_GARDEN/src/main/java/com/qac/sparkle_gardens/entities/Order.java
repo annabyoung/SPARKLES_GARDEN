147de0728bfff8508670d30653432c1978392007
Damien Lloyd <damien.lloyd21@gmail.com>
2016-08-03 11:50:27 +0100

    Documentation etc

--- SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/entities/Order.java
+++ SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/entities/Order.java
@@ -1,41 +1,73 @@
 package com.qac.sparkle_gardens.entities;
 
+/**
+ * 
+ * @author Damien Lloyd
+ *
+ */
 public class Order 
 {
 	private String orderID;
 	private int quantity;
 	private int priceTotal;
 	
+	/**
+	 * Default constructor
+	 */
 	public Order()
 	{
 		
 	}
-
+	
+	/**
+	 * Get the order ID
+	 * @return orderID
+	 */
 	public String getOrderID() 
 	{
 		return orderID;
 	}
-
+	
+	/**
+	 * Set the order ID
+	 * @param orderID
+	 */
 	public void setOrderID(String orderID) 
 	{
 		this.orderID = orderID;
 	}
-
+	
+	/**
+	 * Get number of items in order
+	 * @return quantity
+	 */
 	public int getQuantity() 
 	{
 		return quantity;
 	}
-
+	
+	/**
+	 * Set number of items in order
+	 * @param quantity
+	 */
 	public void setQuantity(int quantity) 
 	{
 		this.quantity = quantity;
 	}
-
+	
+	/**
+	 * Get total price from products in order
+	 * @return priceTotal
+	 */
 	public int getPriceTotal() 
 	{
 		return priceTotal;
 	}
-
+	
+	/**
+	 * Set the price 
+	 * @param priceTotal
+	 */
 	public void setPriceTotal(int priceTotal) 
 	{
 		this.priceTotal = priceTotal;
--- SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/entities/OrderLine.java
+++ SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/entities/OrderLine.java
@@ -1,16 +1,29 @@
 package com.qac.sparkle_gardens.entities;
 
+/**
+ * 
+ * @author Damien Lloyd
+ *
+ */
 public class OrderLine 
 {
 	private String orderID;
 	private String serialNumber;
 	
+	/**
+	 * Default constructor
+	 */
 	OrderLine()
 	{
 		this.orderID = "";
 		this.serialNumber = "";
 	}
 	
+	/**
+	 * Create OrderLine from Product and Order
+	 * @param p The product
+	 * @param o The order
+	 */
 	OrderLine(Product p, Order o)
 	{
 		this.orderID = o.getOrderID();
