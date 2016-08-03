b89a228e9e6e0ccf46a9c855fc75c1121a1de1e3
Damien Lloyd <damien.lloyd21@gmail.com>
2016-08-03 10:04:24 +0100

    adding initial project

---
+++ sparkle_gardens/src/main/java/com/qac/sparkle_gardens/App.java
@@ -0,0 +1,13 @@
+package com.qac.sparkle_gardens;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
