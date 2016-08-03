2f0f7a88a967d3136b30aabbf7f0b16b6352c1e1
Annabelle Young <annabyoung@gmail.com>
2016-08-03 11:32:48 +0100

    Product Getters and Setters

--- SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/entities/Product.java
+++ SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/entities/Product.java
@@ -1,6 +1,33 @@
 package com.qac.sparkle_gardens.entities;
 
 public class Product {
+	private String productName, serialNumber;
+	private int stockLevel;
+	private float price;
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+	public int getStockLevel() {
+		return stockLevel;
+	}
+	public void setStockLevel(int stockLevel) {
+		this.stockLevel = stockLevel;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
 	
 	
 	
