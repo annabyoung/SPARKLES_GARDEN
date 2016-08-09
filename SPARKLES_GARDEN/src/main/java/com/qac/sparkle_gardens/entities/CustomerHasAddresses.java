ae0da94887179e9e1d291dfeb79b75bf787e861f
Tyler Deans <tyler@geekingdoing.com>
2016-08-09 17:30:13 +0000

    Created Customer Address composite entity

---
+++ SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/entities/CustomerHasAddresses.java
@@ -0,0 +1,41 @@
+package com.qac.sparkle_gardens.entities;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ * 
+ * @author Tyler Deans
+ *
+ */
+public class CustomerHasAddresses {
+	// Composite key
+	@Id
+	@JoinColumn (name="customer_fk", nullable = false)
+	@ManyToOne
+	private Customer customer;
+	
+	@Id
+	@JoinColumn (name="address_fk", nullable = false)
+	@ManyToOne 
+	private Address address;
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+}
