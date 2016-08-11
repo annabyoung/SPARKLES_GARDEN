ca60bff09394d27779adf5776ade61e345cc1174
Damien Lloyd <damien.lloyd21@gmail.com>
2016-08-11 14:27:07 +0100

    Created Invoice

--- Sparkles_Account/src/main/java/com/qac/sparkles_accounts/entities/Invoice.java
+++ Sparkles_Account/src/main/java/com/qac/sparkles_accounts/entities/Invoice.java
@@ -1,12 +1,52 @@
 package com.qac.sparkles_accounts.entities;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
 import com.qac.sparkle_gardens.entities.Customer;
 import com.qac.sparkle_gardens.entities.Order;
+import com.qac.sparkle_gardens.entities.OrderLine;
 
-public class Invoice 
+public class Invoice implements Serializable
 {
+	/**
+	 * Serial ID of Invoice
+	 */
+	private static final long serialVersionUID = 2259219866064562568L;
+	
 	Customer customer;
 	Order order;
 	
-	Invoice
+	public Invoice()
+	{
+		
+	}
+	
+	public Invoice(Customer customer, Order order)
+	{
+		this.customer = customer;
+		this.order = order;
+	}
+	
+	public String generate()
+	{
+		String invoice = "";
+		ArrayList<OrderLine> lines = order.getOrderLines();
+		
+		invoice += "\n\n\n----------------------------------------------";
+		
+		invoice += "Thank you for shopping at NBGardens!\n";
+		invoice += "You have purchased the following items: \n";
+		
+		for (OrderLine i : lines)
+		{
+			invoice += "\n\nProduct = " + i.getProduct().getProductName() + "\n" + 
+						"   Quantity = " + i.getQuantity() + "\n" + 
+						    "Price = " + (i.getProduct().getPrice() * i.getQuantity()) + "\n\n";
+		}
+				
+		invoice += "\n\n\n----------------------------------------------";
+		
+		return invoice;
+	}
 }
