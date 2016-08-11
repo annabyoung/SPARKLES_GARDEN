5fa3ae7a307dfb3a67588ef7396cb904ca762211
Tyler Deans <tyler@geekingdoing.com>
2016-08-11 16:13:17 +0000

    Started on message receiver method for credit check

---
+++ Sparkles_Account/src/main/java/com/qac/sparkles_accounts/entities/CreditCheck.java
@@ -0,0 +1,38 @@
+package com.qac.sparkles_accounts.entities;
+
+import static javax.jms.Session.AUTO_ACKNOWLEDGE;
+
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueReceiver;
+import javax.jms.QueueSession;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class CreditCheck {
+	private Context context= null;
+	private QueueConnectionFactory conFact= null;
+	private QueueConnection connect = null;
+	private QueueSession session = null;
+	private QueueReceiver receiver = null;
+	private Queue requests = null;
+	
+	public void receiveCustomerMessage() {
+		try {
+			context = new InitialContext();
+			conFact = (QueueConnectionFactory) context.lookup("ConnectionFactory");
+			requests = (Queue) context.lookup("queue1");
+			connect = conFact.createQueueConnection();
+			session = connect.createQueueSession(false, AUTO_ACKNOWLEDGE);
+		} catch (NamingException e){
+			
+		} catch (JMSException e) {
+			
+		} finally {
+			
+		}
+	}
+}
