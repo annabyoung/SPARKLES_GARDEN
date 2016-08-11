4f769354e7526cbb2a7a2fba3a879c7c9f4b53d0
CGurell <chris.gurrell@gmail.com>
2016-08-11 14:12:37 +0000

    Added web.xml

--- SPARKLES_GARDEN/src/main/webapp/js/formReplication.js
+++ SPARKLES_GARDEN/src/main/webapp/js/formReplication.js
@@ -1,8 +1,6 @@
-<script language="javascript">
-
 function showNewForm() {
 	
-	//var addressForm = document.getElementById("formdiv");
+	var addressForm = document.getElementById("formdiv");
 	
 	document.getElementById("addExtraForm").innerHTML = '<label>House number/ name:</label><input type="text" name="houseName"><br><label>Address Line 1:</label><input type="text" name="ad1"><br><label>Address Line 2:</label><input type="text" name="ad2"><br><label>City:</label><input type="text" name="city"><br><label>County:</label><input type="text" name="county"><br><label>Country:</label><input type="text" name="country"><br><label>Postcode:</label><input type="text" name="postcode"><br><label>Address Type:</label><select><option value="House">Billing</option><option value="Apartment">Shipping</option><br></select><span id="addExtraForm"></span>'
 	
