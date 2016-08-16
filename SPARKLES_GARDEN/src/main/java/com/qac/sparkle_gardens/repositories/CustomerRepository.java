7013ce1e587a812401d580b8ca44a4fc8b68eb9c
seannelly <sean.connelly@qa.com>
2016-08-16 15:16:13 +0000

    customer testing and tweaks

--- SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/repositories/CustomerRepository.java
+++ SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/repositories/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.qac.sparkle_gardens.repositories;
 
 import java.util.List;
 
+import com.qac.sparkle_gardens.entities.Address;
+import com.qac.sparkle_gardens.entities.Card;
 import com.qac.sparkle_gardens.entities.Customer;
 import com.qac.sparkle_gardens.util.MethodAuthor;
 
@@ -21,8 +23,8 @@ public interface CustomerRepository {
 	public Customer getCustomer(Customer c); //get customer
 	public void updateCustomer(Customer c); //update customer what am i updating tho? I may need more of these 
 	public void removeCustomer(Customer c); //remove customer
-	public void findCustomerAdresses(Customer c);
-	public void findCustomerCards(Customer c);
+	public Address findCustomerAddresses(Customer c);
+	public Card findCustomerCards(Customer c); //maybe get composite class? 
 	
 	/**
 	 * Finds a customer by their email address and returns null is no customer is found
---
+++ SPARKLES_GARDEN/src/test/java/com/qac/sparkle_gardens/services/CustomerServiceTest.java
@@ -0,0 +1,84 @@
+package com.qac.sparkle_gardens.services;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import com.qac.sparkle_gardens.entities.Card;
+import com.qac.sparkle_gardens.entities.Customer;
+import com.qac.sparkle_gardens.repositories.CustomerRepository;
+import com.qac.sparkle_gardens.util.CreditStatus;
+import com.qac.sparkle_gardens.controllers.CustomerInterface;
+
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ * @author sean connelly 
+ *
+ */
+
+public class CustomerServiceTest extends TestCase {
+	
+	@Inject 
+	CustomerRepository customerRepository; 
+	Customer dummyCustomer =  new Customer("John", "Smith", "email@email.com", CreditStatus.ONHOLD, "password01", "1234567890"); 
+	
+	public void persistCustomerTest(Customer customer){
+	
+		  customerRepository.persistCustomer(customer);	
+	}
+	
+	public void persistCustomerTest(List<Customer> customers){
+	
+		customerRepository.persistCustomer(customers);
+	}
+	
+	public void  findByIDTest(long accountID){
+		
+		Customer result = customerRepository.findByID(accountID);
+		assertNotNull(result);
+	}
+	
+	public void getCustomerTest(Customer c){
+		
+		Customer customer = customerRepository.getCustomer(c); 
+		assertNotNull(customer);
+		assertEquals(customer, dummyCustomer);
+	}
+		
+	public void updateCustomerTest(Customer c){
+
+		customerRepository.updateCustomer(c); //update customer what am i updating tho? I may need more of these 
+	
+	}
+		
+	
+	public void removeCustomerTest(Customer c){
+		customerRepository.removeCustomer(c); //remove customer
+	}
+	
+	public void findCustomerAddressesTest(Customer c){
+	
+		customerRepository.findCustomerAddresses(c);
+	}
+		
+		
+	public void findCustomerCardsTest(Customer c){
+		
+		Card cards = customerRepository.findCustomerCards(c);
+		assertNotNull(cards);
+		
+	}
+	
+	public Customer findByEmail(String email){
+
+		
+	   return customerRepository.findByEmail(email);
+	
+	}
+}
