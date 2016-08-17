b0f2a3ddcd7bc0980dad5c0fb84d17212cdf4bb5
lendayem <damien.lloyd21@gmail.com>
2016-08-17 15:31:28 +0100

    More unit tests!

--- SPARKLES_GARDEN/src/test/java/com/qac/sparkle_gardens/repositories/offline/OrderRepositoryOfflineTest.java
+++ SPARKLES_GARDEN/src/test/java/com/qac/sparkle_gardens/repositories/offline/OrderRepositoryOfflineTest.java
@@ -1,26 +1,64 @@
 package com.qac.sparkle_gardens.repositories.offline;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.qac.sparkle_gardens.entities.Order;
 import com.qac.sparkle_gardens.entities.OrderLine;
-import com.qac.sparkle_gardens.entities.Product;
+import com.qac.sparkle_gardens.repositories.OrderRepository;
+import com.sparkle_gardens.sample_data.OrderSamples;
 
 public class OrderRepositoryOfflineTest 
 {
-	OrderRepositoryOffline repository;
+	//OrderRepositoryOffline repository;
+	OrderRepository or;
 	
+	@Before
+	public void initialise()
+	{
+		//repository = new OrderRepositoryOffline();
+		or = new OrderRepositoryOffline();
+	}
+	
+	@Test
 	public void successfulPersist()
 	{
-		Order o = new Order();
-		o.addOrderLine(new OrderLine(new Product("Great shoes", 5, 61.99), 1));
-		o.addOrderLine(new OrderLine(new Product("Fun facts", 10, 8.00), 2));
-		o.addOrderLine(new OrderLine(new Product("The challenge", 1, 79.99), 1));
+		Order o = OrderSamples.kinky();
+
 		
-		repository.persistOrder(o);
+		or.persistOrder(o);
 		
-		assertEquals(repository.getOrder(o.getOrderID()), o);
+		assertEquals(or.getOrder(o.getOrderID()), o);
 	}
+	
+	@Test
+	public void orderEmpty()
+	{
+		Order o = OrderSamples.food();
+		
+		assertThat(o, is(notNullValue()));
+	}
+	
+	@Test
+	public void verifyQuantity()
+	{
+		for (Order o : OrderSamples.orders())
+		{
+			for (OrderLine ol : o.getOrderLines())
+			{
+				assertFalse(ol.getQuantity() == 0);
+			}
+		}
+	}
+	
+	@After
+	public void teardown(){
+	//	repository = null;
+		or = null;
+	}
+	
 }
