73280f61f6f9b45d042d157571680e5eecda6cb4
LukeDSmith <lukedsmith93@gmail.com>
2016-08-17 16:10:04 +0100

    more restrictions on input during registration

---
+++ SPARKLES_GARDEN/src/main/webapp/js/userValidation.js
@@ -0,0 +1,26 @@
+function Validation() {
+	
+	var firstNCheck = document.getElementById('formdiv:firstName');
+	var surnameCheck = document.getElementById('formdiv:surname');
+	var emailCheck = document.getElementById('formdiv:email');
+	var emailConCheck = document.getElementById('formdiv:emailCon');
+	var passwordCheck = document.getElementById('formdiv:password');
+	var passwordConCheck = document.getElementById('formdiv:passCon');
+	
+	var error=""; 
+	
+	var variablesToCheck = [firstNCheck, surnameCheck, emailCheck, emailConCheck, passwordCheck, passwordConCheck];
+	
+	for(i=0; i < variablesToCheck.length; i++){
+		if(variablesToCheck[i].value == null || variablesToCheck[i].value == ""){
+			
+			error="All fields must be filled in before continuing"
+			document.getElementById("errorMessage").innerHTML= error;
+			return false;
+			
+		}
+	}
+	
+	window.location=('../registerAddress.xhtml');
+	return true;
+}
