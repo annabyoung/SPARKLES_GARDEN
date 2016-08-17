d1c05093187527693a26f32d876adc065bdd16c4
CGurrell <chris.gurrell@gmail.com>
2016-08-17 16:22:08 +0100

    EOD Push

--- SPARKLES_GARDEN/src/main/webapp/js/basket.js
+++ SPARKLES_GARDEN/src/main/webapp/js/basket.js
@@ -1,3 +1,29 @@
-function deleteItem(rowNumber){
-	document.getElementById("basketTable").deleteRow("rowNumber");
+function deleteItem(){
+	/**
+	 * Call getProductByID in ProductRepository to get the Product p
+	 * Call removeProductFromBasket from com.qac.sparkle_gardens.services.OrderService using p
+	 */
+}
+
+function addItem(){
+	/**
+	 * Call getProductByID in ProductRepository to get the Product p
+	 * Call addProductToBasket from com.qac.sparkle_gardens.services.OrderService using p
+	 */ 
+}
+function printTable(){
+	/**
+	 * Call isBasketEmpty from com.qac.sparkle_gardens.services.OrderService. If true, simply print a "Your basket is Empty" message. 
+	 * Else
+	 * Call getBasket from com.qac.sparkle_gardens.services.OrderService
+	 * Retrieve Product image, name, desc and price from= the basket
+	 * Find all orders with duplicate products, create one line with a quantity >1
+	 * Set all other quantities to 1
+	 * Print to screen
+	 */
+}
+function clearBasket(){
+	/**
+	 * Call clearBasket from com.qac.sparkle_gardens.services.OrderService
+	 */ 
 }
