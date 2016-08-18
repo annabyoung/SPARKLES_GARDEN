06d18e6ec3e08f31dfb5c92f8699545c7637d163
CGurrell <chris.gurrell@gmail.com>
2016-08-18 16:07:08 +0100

    Homepage scripting

---
+++ SPARKLES_GARDEN/src/main/webapp/js/homepage.js
@@ -0,0 +1,17 @@
+function addProductToHomepage(imageNumber){
+	var elem = document.createElement("img");
+	var srcLink = ("img/gnome"+imageNumber+".jpg");
+	elem.setAttribute("src", srcLink);
+	elem.setAttribute("height", "160");
+	elem.setAttribute("alt", "");
+	var elemId = ("imageInsertLocation" + imageNumber);
+	var x = document.getElementById(elemId);
+	x.appendChild(elem);
+}
+
+function addAllProducts(){
+	var i=0;
+	for (i=0;i<5;i++){
+		addProductToHomepage(i);
+	}
+}
