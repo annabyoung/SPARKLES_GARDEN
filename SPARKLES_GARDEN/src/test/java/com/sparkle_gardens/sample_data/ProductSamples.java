40f20312eca07de4179085b97d878ed089b38449
lendayem <damien.lloyd21@gmail.com>
2016-08-18 11:59:50 +0100

    Created ProductSamples class

---
+++ SPARKLES_GARDEN/src/test/java/com/sparkle_gardens/sample_data/ProductSamples.java
@@ -0,0 +1,71 @@
+package com.sparkle_gardens.sample_data;
+
+import com.qac.sparkle_gardens.entities.Product;
+
+public class ProductSamples 
+{
+	public static Product greatShoes()
+	{
+		return new Product("Great shoes", 5, 61.99);
+	}
+	
+	public static Product nikeTrainers()
+	{
+		return new Product("Nike trainers", 10, 82.00);
+	}
+	
+	public static Product lilBoots()
+	{
+		return new Product("'lil boots!", 1, 79.99);
+	}
+	
+	public static Product vegasHeelz()
+	{
+		return new Product("Vegas heelz", 3, 120.00);
+	}
+	
+	public static Product maximusLube()
+	{
+		return new Product("Maximus lube", 10, 10.99);
+	}
+	
+	public static Product dildoInch8()
+	{
+		return new Product("8\" dildo", 4, 49.99);
+	}
+	
+	public static Product vibeRing()
+	{
+		return new Product("Vibe ring", 20, 5.99);
+	}
+	
+	public static Product exstaticSqueeze()
+	{
+		return new Product("Exstatic squeeze", 2, 9.49);
+	}
+	
+	public static Product fleshTorch()
+	{
+		return new Product("Flesh torch", 30, 29.99);
+	}
+	
+	public static Product mango()
+	{
+		return new Product("Mango", 20, 1.00);
+	}
+	
+	public static Product watermelon()
+	{
+		return new Product("Watermelon", 10, 10.00);
+	}
+	
+	public static Product cucumber()
+	{
+		return new Product("Cucumber", 20, 0.49);
+	}
+	
+	public static Product fishNChips()
+	{
+		return new Product("Fish 'n Chips", 10, 4.99);
+	}
+}
