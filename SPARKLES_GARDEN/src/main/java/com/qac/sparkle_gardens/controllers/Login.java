31df534db9d2a63c68eb6099d14719d2c525637b
CGurell <chris.gurrell@gmail.com>
2016-08-22 13:03:34 +0000

    Login Scripting in Progress

--- SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/controllers/Login.java
+++ SPARKLES_GARDEN/src/main/java/com/qac/sparkle_gardens/controllers/Login.java
@@ -17,16 +17,19 @@ import com.qac.sparkle_gardens.services.CustomerService;
 
 @Named (value ="Login")
 public class Login {
-	
-CustomerService services;  
 
-String email="";
-String password="";
+	CustomerService services;  
 
-//validate emails
-long userID = services.getUserIDAtLogin(email, password); 
-
-
+	String email="";
+	String password="";
+	String didItWork="Did not work";
+	//validate emails
+	long userID = services.getUserIDAtLogin(email, password); 
+	public void beanCheck(String e, String p){
+		System.out.println("Email is: " + e);
+		System.out.println("Password is: " + p);
+		didItWork="It Worked!";
+	}
 
 
 }
--- SPARKLES_GARDEN/src/main/webapp/js/loginValidation.js
+++ SPARKLES_GARDEN/src/main/webapp/js/loginValidation.js
@@ -6,18 +6,13 @@ function loginValidation() {
 	var error=""; 
 	
 	var variablesToCheck = [usernameCheck, passwordCheck];
-	
 	for(i=0; i < variablesToCheck.length; i++){
 		if(variablesToCheck[i].value == null || variablesToCheck[i].value == ""){
-			
-
 			error="Please enter a valid username and password"
 			document.getElementById("errorMessage").innerHTML= error;
 			return false;
-			
 		}
 	}
-	
 	window.location=('../homepage.xhtml');
 	return true;
 }
