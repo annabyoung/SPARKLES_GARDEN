9669c6ad7cffc2080ad89e78ef4b258349c6380f
LukeDSmith <lukedsmith93@gmail.com>
2016-08-23 15:22:18 +0000

    meh

--- SPARKLES_GARDEN/src/main/webapp/js/typeValidationCard.js
+++ SPARKLES_GARDEN/src/main/webapp/js/typeValidationCard.js
@@ -1,10 +1,25 @@
-$(function(){
+/*$(function(){
 	$("#submit").click(function(){
-		var number = document.getElementById("#cardNumber").validity.typeMismatch;
+		var number = document.getElementById("#cardNumber");
 		if(number.validity.typeMismatch){
 			number.setCustomValidity("Data type mismatch");
 		} else{
 			number.setCustomValidity("");
+		}
+	});
+});*/
+
+$(function(){
+	$("#submit").click(function(){
+		var valueToCheck = document.getElementById("#cardNumber").value;
+		var validValue = new RegExp ('^4[0-9]{12}(?:[0-9]{3})?$');
+		
+		if(validValue.test(valueToCheck)){
+			
+			alert('Valid card number');
+		}else{
+			
+			alert('Please enter a valid visa card number');
 		}
 	});
 });
