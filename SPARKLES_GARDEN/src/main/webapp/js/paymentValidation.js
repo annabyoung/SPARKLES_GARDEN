4821c168a473cc17bbd0a62d66becf4744fdf6f5
CGurell <chris.gurrell@gmail.com>
2016-08-24 11:38:49 +0000

    conflict resolved

--- SPARKLES_GARDEN/src/main/webapp/js/paymentValidation.js
+++ SPARKLES_GARDEN/src/main/webapp/js/paymentValidation.js
@@ -1,23 +1,16 @@
 function cardValidation() {
-	
 	var nameCheck = document.getElementById('cardName');
 	var numCheck = document.getElementById('cardNumber');
 	var expCheck = document.getElementById('expDate');
-	
 	var error="";
-	
 	var variablesToCheck = [nameCheck, numCheck, expCheck];
-	
 	for(i=0; i < variablesToCheck.length; i++){
 		if(variablesToCheck[i] == null || variablesToCheck[i].value == ""){
-			
 			error="All fields marked with * must be filled in"
 			document.getElementById("errorMessage").innerHTML= error;
 			return false;
-			
 		}
 	}
-	
 	window.location=('../homepage.xhtml');
 	return true;
 }
