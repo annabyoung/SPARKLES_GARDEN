84cc99edd83e788d608131fb94d7e1f06911202d
CGurell <chris.gurrell@gmail.com>
2016-08-25 17:27:18 +0000

    End of day commit 25-8

--- SPARKLES_GARDEN/src/main/webapp/js/homepage.js
+++ SPARKLES_GARDEN/src/main/webapp/js/homepage.js
@@ -29,24 +29,61 @@ function addAllProducts(productList){
 function getListOfProducts(){
 	var listOfProducts;
 
-	listOfProducts = $.getJSON("/api/search/product").done(function getTheList(theData){
+	listOfProducts = $.getJSON("/api/search/productList").done(function getTheList(theData){
 
 		theData = listOfProducts;
 		console.log("Info Retrieved is " + theData); 
 		console.log(Object.keys(listOfProducts))
-		
+
 		console.log(theData);
-		
+
 		var theJSONObject = listOfProducts["responseText"];
-		console.log("theJSONObject = " +theJSONObject);
+		theJSONObject = theJSONObject.split("},");
+		
 		var key = "productName";
 		var prod = {productDefinition:theJSONObject}
-		console.log("Product ID = " + (prod["productDefinition"].productID));
-		
-
-//		addAllProducts(listOfProducts);
-//		console.log("addAllProducts called");
+		console.log(theJSONObject);
+		for (var i in theJSONObject){
+			theJSONObject[i] = theJSONObject[i].split("}]");
+			getDataElement(theJSONObject[i][0]);
+		}
 	});
+}
+function getDataElement(product){
+	console.log("In getDataElement");
+	console.log(product);
+	
+	
+	var element = [0,"7",3,4,5,6] ;
+	var i = 0;
+	var res = product.split(',');
+	for (var i=0;i<res.length;i++){
+		res[i] = res[i].split(":");
 
-
+		console.log("res[" + i + "] = " + res[i]);
+	}
+	console.log("res = " + res);
+	
+	res[(res.length) -1][0] = res[(res.length) -1][0].split("]");
+	var finalTag = res[(res.length) -1][0][0];
+	var prodID = res[0][1];
+	var name = res[1][1];
+	var stock = res[2][1];
+	var cost = res[3][1];
+	var desc = res[4][1];
+	console.log("Product ID = " + prodID);
+	console.log("Name = " + name);
+	console.log("Stock = " + stock);
+	console.log("Price = " + cost);
+	console.log("Description = " + desc);
+	console.log(res);
+	res[5][1]= res[5][1].split("[");
+	var firstTag = res[5][1][1];
+	var tags = firstTag;
+	var i = 6;
+	for (i=6;i<res.length-1;i++){
+		tags+=res[i];
+	}
+	tags+=finalTag;
+	console.log("Tags = " + tags);
 }
